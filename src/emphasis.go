0e60e86e5d98901ab3e2fd2bb9955fdd797df2dd
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-19 13:29:26 -0400

    Make emphasis styles inlineable

--- src/emphasis.go
+++ src/emphasis.go
@@ -6,11 +6,15 @@ type Emphasis struct {
 	text InlineStyle
 }
 
+func (Emphasis) canInline() {}
+
 func (e Emphasis) Markdown() string {
 	return fmt.Sprintf("*%v*", e.text.Markdown())
 }
 
 type StrongEmphasis Emphasis
+
+func (StrongEmphasis) canInline() {}
 
 func (s StrongEmphasis) Markdown() string {
 	return fmt.Sprintf("**%v**", s.text.Markdown())
