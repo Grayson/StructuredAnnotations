d9e3c2ac404d9378d9cc5cfbde636bb5f9f2c555
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-19 13:33:34 -0400

    Make `Annotations` conform to ConvertableToMarkdown

--- src/annotations.go
+++ src/annotations.go
@@ -1,9 +1,25 @@
 package structuredannotations
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Annotations struct {
 	styles []ConvertableToMarkdown
 }
 
 func (a *Annotations) Add(styles ...ConvertableToMarkdown) {
 	a.styles = append(a.styles, styles...)
+}
+
+func (a *Annotations) Markdown() string {
+	var sb strings.Builder
+
+	for _, style := range a.styles {
+		fmt.Fprint(&sb, style.Markdown())
+		fmt.Fprint(&sb, "\n\n")
+	}
+
+	return strings.Trim(sb.String(), "\n")
 }
--- src/annotations_test.go
+++ src/annotations_test.go
@@ -1,6 +1,8 @@
 package structuredannotations
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAnnotations_Add(t *testing.T) {
 	tests := []struct {
@@ -32,6 +34,33 @@ func TestAnnotations_Add(t *testing.T) {
 			a.Add(tt.args...)
 			if len(a.styles) != len(tt.args) {
 				t.Error("Unmatched number of styles!")
+			}
+		})
+	}
+}
+
+func TestAnnotations_Markdown(t *testing.T) {
+	tests := []struct {
+		name   string
+		styles []ConvertableToMarkdown
+		want   string
+	}{
+		{
+			"Annotations Basic Markdown",
+			[]ConvertableToMarkdown{
+				Text("test"),
+				CreateParagraph(Text("test2"), Emphasis{Text("test3")}),
+			},
+			"test\n\ntest2 *test3*",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Annotations{
+				styles: tt.styles,
+			}
+			if got := a.Markdown(); got != tt.want {
+				t.Errorf("Annotations.Markdown() = %v, want %v", got, tt.want)
 			}
 		})
 	}
