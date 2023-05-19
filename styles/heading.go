ef6381b8dd9eda3c2e6deca77e35f79f14af6f8f
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-19 10:59:48 -0400

    Add `Heading` style

---
+++ styles/heading.go
@@ -0,0 +1,26 @@
+package structuredannotations
+
+import (
+	"fmt"
+	"strings"
+)
+
+type HeadingLevel int
+
+const (
+	H1 HeadingLevel = iota
+	H2
+	H3
+	H4
+	H5
+	H6
+)
+
+type Heading struct {
+	text  string
+	level HeadingLevel
+}
+
+func (h *Heading) Markdown() string {
+	return fmt.Sprintf("%v %v", strings.Repeat("#", int(h.level)+1), h.text)
+}
---
+++ styles/heading_test.go
@@ -0,0 +1,43 @@
+package structuredannotations
+
+import "testing"
+
+func TestHeading_Markdown(t *testing.T) {
+	type fields struct {
+		text  string
+		level HeadingLevel
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			"H1 Test",
+			fields{
+				"test",
+				H1,
+			},
+			"# test",
+		},
+		{
+			"H6 Test",
+			fields{
+				"test",
+				H6,
+			},
+			"###### test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &Heading{
+				text:  tt.fields.text,
+				level: tt.fields.level,
+			}
+			if got := h.Markdown(); got != tt.want {
+				t.Errorf("Heading.Markdown() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
