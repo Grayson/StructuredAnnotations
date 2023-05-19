03ab85a75e58676b1750383f73e470ddbad5b226
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-19 12:52:37 -0400

    Add (and ignore) line breaks

---
+++ styles/linebreak.go
@@ -0,0 +1,14 @@
+package structuredannotations
+
+// Currently unsupported
+// LineBreaks can appear in paragraphs, emphasis, links, and other tags that
+// allow inline styling.  Adding them would break the current API and requires
+// additional consideration.
+
+// type LineBreak struct{}
+
+// func (LineBreak) Markdown() string {
+// 	return "  "
+// }
+
+// func (LineBreak) canInline() {}
