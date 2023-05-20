c07e0473fd81126dec7929e63732469d8b682801
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-19 20:49:51 -0400

    Update demo

--- demo/demo.go
+++ demo/demo.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	sa "github.com/grayson/structuredannotations/src"
 )
 
-func main() {
+func basicStructuredCode() {
 	a := sa.NewAnnotation(sa.WithStyle(sa.Info))
 
 	a.Add(
@@ -30,4 +31,50 @@ func main() {
 	fmt.Println(a.Markdown())
 	err := a.Send()
 	fmt.Printf("error: %v\n", err)
+}
+
+func templateWithSwappable() {
+	a := sa.NewAnnotation(sa.WithStyle(sa.Warning), sa.WithUniqueContext())
+
+	t, _ := sa.NewTemplate(`
+# This is a templated annotation!
+
+We're going to populate the following list:
+
+{{.list}}
+
+It shoudl update dynamically as you watch!
+`)
+	a.Add(t)
+
+	swap := sa.CreateSwappable(sa.Text("This will be swapped!"))
+
+	list := sa.CreateOrderedList(
+		sa.Text("First item"),
+		swap,
+		sa.Text("Last item"),
+	)
+	t.Add("list", list)
+
+	<-time.NewTimer(3 * time.Second).C
+	swap.Swap(sa.Text("Swapped for text!"))
+	err := a.Send()
+	fmt.Printf("error: %v\n", err)
+
+	<-time.NewTimer(3 * time.Second).C
+	swap.Delete()
+	err = a.Send()
+	fmt.Printf("error: %v\n", err)
+
+	<-time.NewTimer(3 * time.Second).C
+	swap.Swap(sa.Text("This was deleted and restored!"))
+
+	fmt.Println(a.Markdown())
+	err = a.Send()
+	fmt.Printf("error: %v\n", err)
+}
+
+func main() {
+	basicStructuredCode()
+	templateWithSwappable()
 }
