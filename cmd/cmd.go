66263df956a17d71ace4569638007892ad9686d6
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-23 10:33:54 -0400

    Add quick demo CLI for templating from the command line

---
+++ cmd/cmd.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	sa "github.com/grayson/structuredannotations/src"
+)
+
+func main() {
+	templateBytes, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	template, err := sa.NewTemplate(string(templateBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, env := range os.Environ() {
+		tmp := strings.SplitN(env, "=", 2)
+		template.Add(tmp[0], sa.Text(tmp[1]))
+	}
+
+	for _, args := range os.Args {
+		tmp := strings.SplitN(args, "=", 2)
+		if len(tmp) < 2 {
+			continue
+		}
+		template.Add(tmp[0], sa.Text(tmp[1]))
+	}
+
+	a := sa.NewAnnotation(sa.WithStyle(sa.Default))
+	a.Add(template)
+	fmt.Println(a.Markdown())
+	err = a.Send()
+	fmt.Printf("error: %v\n", err)
+}
